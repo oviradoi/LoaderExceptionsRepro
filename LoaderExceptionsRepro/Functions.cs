7c8a0a5f6d204b3133617c227692bc7dbc766407
Ovidiu Radoi <oradoi@talentsoft.com>
2020-04-22 09:37:33 +0300

    Add Functions.cs and implement Main method

---
+++ LoaderExceptionsRepro/Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace LoaderExceptionsRepro
+{
+	public static class Functions
+    {
+        [NoAutomaticTrigger]
+        public static void TestFunction()
+        {
+        }
+    }
+}
--- LoaderExceptionsRepro/Program.cs
+++ LoaderExceptionsRepro/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace LoaderExceptionsRepro
 {
@@ -10,6 +12,15 @@ namespace LoaderExceptionsRepro
     {
         static void Main(string[] args)
         {
+            var builder = new HostBuilder();
+            builder.ConfigureLogging((context, b) => b.AddConsole());
+            builder.ConfigureWebJobs(b => b.AddAzureStorage());
+
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
         }
     }
 }
